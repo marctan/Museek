e47c5094cc4cade077d45abc09f5da33d2687f57
Marc Tan <marcqtan@gmail.com>
2020-05-08 16:41:00 +0800

    Add animation and change list to recyclerview

--- app/src/main/java/com/example/marcqtan/samplemusic/MainActivity.java
+++ app/src/main/java/com/example/marcqtan/samplemusic/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -73,23 +75,40 @@ import java.util.List;
  * Created by Marc Q. Tan on 17/04/2020.
  */
 
-public class MainActivity extends AppCompatActivity {
-    ListView lv;
+public class MainActivity extends AppCompatActivity implements SongAdapter.OnItemClickListener {
+    RecyclerView rv;
     MediaControllerCompat mediaControllerCompat;
     MediaSessionCompat.Token token;
     ImageView prev, next, album_artwork, credits;
-    CustomAdapter adapter;
+    SongAdapter adapter;
     SeekBar seekBar;
     TextView start, end, title;
     Handler handler;
     SeekBarRunnable runnable;
     private PlaybackStateCompat mLastPlaybackState;
-    ProgressBar progressBar, fetchTrack;
+    ProgressBar progressBar;
 
     MediaControllerCallback mediaControllerCallback;
 
     SessionTokenBroadCastReceiver sessionReceiver;
     TrackBroadCastReceiver trackReceiver;
+
+    @Override
+    public void onItemClick(int position) {
+        MediaControllerCompat controller = MediaControllerCompat.getMediaController(MainActivity.this);
+        MediaDescriptionCompat description = controller.getMetadata().getDescription();
+        String mediaId = TrackModel.getTrackModels().get(position).id;
+
+        if (!mediaId.equals(description.getMediaId())) {
+            controller.getTransportControls().playFromMediaId(mediaId, null);
+            return;
+        }
+
+        Intent intent = new Intent(MainActivity.this, SongPage.class);
+        intent.putExtra("media_description", description);
+        intent.putExtra("token", token);
+        startActivity(intent);
+    }
 
     private class MediaControllerCallback extends MediaControllerCompat.Callback {
         @Override
@@ -117,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         }
         title.setText(description.getTitle());
         String url = metadata.getString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI);
-        if(url != null) {
+        if (url != null) {
             Glide.with(this).load(url).into(album_artwork);
         }
     }
@@ -199,9 +218,8 @@ public class MainActivity extends AppCompatActivity {
     private class TrackBroadCastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if("tracks".equals(intent.getAction())) {
+            if ("tracks".equals(intent.getAction())) {
                 adapter.setItem(TrackModel.getTrackModels());
-                fetchTrack.setVisibility(View.GONE);
             }
         }
     }
@@ -238,7 +256,6 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_music_list);
-        fetchTrack = findViewById(R.id.progressBar2);
 
         mediaControllerCallback = new MediaControllerCallback();
         initBroadCasters();
@@ -246,10 +263,12 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i = new Intent(MainActivity.this, MusicService.class);
         Util.startForegroundService(MainActivity.this, i);
-        lv = findViewById(R.id.lv);
+        rv = findViewById(R.id.rv);
 
-        adapter = new CustomAdapter(this);
-        lv.setAdapter(adapter);
+        adapter = new SongAdapter(this, this, this);
+        rv.setAdapter(adapter);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
 
         progressBar = findViewById(R.id.progressBar);
 
@@ -258,25 +277,6 @@ public class MainActivity extends AppCompatActivity {
         end = findViewById(R.id.end);
         title = findViewById(R.id.songName);
         seekBar.setEnabled(false); //set to true to show
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                MediaControllerCompat controller = MediaControllerCompat.getMediaController(MainActivity.this);
-                MediaDescriptionCompat description = controller.getMetadata().getDescription();
-                String mediaId = TrackModel.getTrackModels().get(i).id;
-
-                if (!mediaId.equals(description.getMediaId())) {
-                    controller.getTransportControls().playFromMediaId(mediaId, null);
-                    return;
-                }
-
-                Intent intent = new Intent(MainActivity.this, SongPage.class);
-                intent.putExtra("media_description", description);
-                intent.putExtra("token", token);
-                startActivity(intent);
-            }
-        });
 
         seekBar.setOnSeekBarChangeListener(new android.widget.SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -376,101 +376,5 @@ public class MainActivity extends AppCompatActivity {
         stopSeekbarUpdate();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(sessionReceiver);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(trackReceiver);
-    }
-
-    private class CustomAdapter extends ArrayAdapter<TrackModel> {
-        private Context ctx;
-        private List<TrackModel> tracks;
-        int selectedIndex;
-        int state;
-
-        public CustomAdapter(@NonNull Context context) {
-            super(context, 0);
-            this.ctx = context;
-            selectedIndex = -1;
-        }
-
-        public void setItem(List<TrackModel> tracks) {
-            this.tracks = tracks;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            if(tracks == null) {
-                return 0;
-            }
-            return tracks.size();
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            View view = convertView;
-            if (view == null) {
-                view = LayoutInflater.from(ctx).inflate(R.layout.song_list_item, parent, false);
-            }
-
-            TrackModel track = tracks.get(position);
-
-            TextView title = view.findViewById(R.id.title);
-            TextView subtitle = view.findViewById(R.id.subtitle);
-            title.setText(track.title);
-            subtitle.setText(track.genre);
-
-            ImageView btn = view.findViewById(R.id.play_pause);
-            ConstraintLayout cl = view.findViewById(R.id.song_detail_cl);
-
-            btn.setImageDrawable(ctx.getDrawable(R.drawable.small_play));
-
-            if (selectedIndex == position) {
-                if (state == PlaybackStateCompat.STATE_PLAYING) {
-                    title.setSelected(true);
-                    cl.setBackground(ctx.getDrawable(R.drawable.timeline));
-                    btn.setImageDrawable(ctx.getDrawable(R.drawable.small_pause));
-                } else {
-                    title.setSelected(false);
-                }
-            } else {
-                cl.setBackground(null);
-                title.setSelected(false);
-            }
-
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (selectedIndex != position) {
-                        MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().playFromMediaId(
-                                tracks.get(position).id, null);
-                        selectedIndex = position;
-                        state = -1; //reset the state when clicking a different row
-                    } else {
-                        int pbState = MediaControllerCompat.getMediaController(MainActivity.this).getPlaybackState().getState();
-                        if (pbState == PlaybackStateCompat.STATE_PLAYING) {
-                            MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().pause();
-                        } else {
-                            MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().play();
-                        }
-                    }
-                }
-            });
-
-            return view;
-        }
-
-        void updateButtonState(int state) {
-            if (this.state != state && (state == PlaybackStateCompat.STATE_PLAYING
-                    || state == PlaybackStateCompat.STATE_PAUSED)) {
-                this.state = state;
-                notifyDataSetChanged();
-            }
-        }
-
-        void updateSelectedIndex(int position) {
-            if (selectedIndex != position) {
-                selectedIndex = position;
-                notifyDataSetChanged();
-            }
-        }
     }
 }
